ca774da7537b86663eb099afe5d8c36e777391ec
Takahashi AKifumi <akipaka@localhost.localdomain>
2020-05-20 11:15:45 +0900

    7th-day

--- config.pl
+++ config.pl
@@ -6,6 +6,9 @@ use Ark::Models '-base';
 my $home = Jobeet::Models->get('home');
 
 return {
+    default_view    => 'MT',
+    active_days     => 30,
+
     database => [
         'dbi:SQLite:' . $home->file('database.db'), '', '',
          {
@@ -13,5 +16,3 @@ return {
          },
     ],
 };
-
-1;
--- lib/Jobeet/Schema/Result/Job.pm
+++ lib/Jobeet/Schema/Result/Job.pm
@@ -3,6 +3,7 @@ use v5.20.3;
 use strict;
 use warnings;
 use parent 'Jobeet::Schema::ResultBase';
+use Jobeet::Models;
 
 __PACKAGE__->table('jobeet_job');
 
@@ -98,6 +99,13 @@ __PACKAGE__->add_columns(
     },
     
 );
+
+sub insert {
+    my $self = shift;
+
+    $self->expires_at( models('Schema')->now->add( days => models('conf')->{active_days} ) );
+    $self->next::method(@_);
+}
 
 __PACKAGE__->set_primary_key('id');
 __PACKAGE__->add_unique_constraint(['token']);
